5520f7ada694435695523e49978afb3a02e74509
Wayne <chevreuilqwerty@gmail.com>
2021-05-25 20:40:37 -0400

    Display list of profiles under profiles list

--- frontend/profileList.js
+++ frontend/profileList.js
@@ -1,12 +1,13 @@
+import ProfileCard from './profileCard';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 const ProfileList = (props) => (
-    <>
-        <div className="header">
-            Profiles
-        </div>
-    </>
+    <div className="header">
+        Profiles
+        {props.profiles.maps(p => (<ProfileCard key={p.id} {...p}/>))}
+    </div>
 )
 
 
