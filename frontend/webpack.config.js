c8693175353de0e83c7bde323993dbbf6a658888
Wayne <chevreuilqwerty@gmail.com>
2021-05-26 15:25:08 -0400

    update configuration with image loader

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i, 
         use: ['style-loader', 'css-loader', 'sass-loader'], 
+      },
+      {
+        test: /\.(svg|png)$/,
+        loader: "file-loader",
       }
     ],
   },
